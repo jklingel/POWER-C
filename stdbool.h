61b40f976aa6736037a192e342918c1f29319e8b
Jan-Arendt Klingel <70195414+jklingel@users.noreply.github.com>
2026-01-21 16:51:43 -0500

    Add files via upload
    POWER C is pre C89 / ANSI C and therefore does not come with a bool data type. This little header file solves this.

---
+++ stdbool.h
@@ -0,0 +1,13 @@
+/* stdbool.h for old C standards */
+/* like POWER C on the C64/C128  */
+/* pre C89 / ANSI C 1989         */
+/* Jan Klingel, 01/2026          */
+
+#define TRUE 1
+#define FALSE 0
+#define bool unsigned
+
+/* Use:                          */
+/* #include <stdbool.h>          */
+/* bool test = TRUE;             */
+
