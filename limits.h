7cbb86817e7e823c9e62f716b26edc18cef5afcc
Jan-Arendt Klingel <70195414+jklingel@users.noreply.github.com>
2026-02-14 18:47:58 -0500

    Add files via upload
    POWER C does not come with a limits.h, so here it is

---
+++ limits.h
@@ -0,0 +1,18 @@
+/* limits.h for POWER C    */
+/* Jan Klingel, 02/2026    */
+
+#define CHAR_BIT 8
+#define CHAR_MIN (-128)
+#define CHAR_MAX 127
+#define SCHAR_MIN (-128)
+#define SCHAR_MAX 127
+#define UCHAR_MIN 0
+#define UCHAR_MAX 255
+#define SHRT_MIN (-32768)
+#define SHRT_MAX 32767
+#define INT_MIN (-32768)
+#define INT_MAX 32767
+#define USHRT_MIN 0
+#define USHRT_MAX 65535
+#define UINT_MIN 0
+#define UINT_MAX 65535
