c3cef0d8f230366bcb9d0eab904d4d28a1eb67d6
Jan-Arendt Klingel <70195414+jklingel@users.noreply.github.com>
2026-02-21 09:07:32 -0500

    Update conio.h
    Updated and sync'ed with conio.o

--- conio.h
+++ conio.h
@@ -1,12 +1,26 @@
-/* conio.h for POWER C           */
-/* This is to provide the        */
-/* clearscr() function from      */
-/* Borland C. Note that POWER C  */
-/* also has no system() function,*/
-/* so system("cls") and system   */
-/* ("clear") are not working     */
-/* Jan Klingel, 01/2026          */
+/* conio.h for POWER C               */
+/* This is to provide the            */
+/* clrscr() etc. functions from      */
+/* Borland C. Note that POWER C      */
+/* also has no system() function,    */
+/* so system("cls") and system       */
+/* ("clear") are not working         */
+/* Jan Klingel, 02/2026              */
 
-clearscr(void) {
-  putchar(147);
-}
+/* Define screen color codes         */
+#define BLACK  0
+#define WHITE  1
+#define RED    2
+#define CYAN   3
+#define PURPLE 4
+#define GREEN  5
+#define BLUE   6
+#define YELLOW 7
+#define ORANGE 8
+#define BROWN  9
+#define LRED   10
+#define DGRAY  11
+#define MGRAY  12
+#define LGREEN 13
+#define LBLUE  14
+#define LGRAY  15
